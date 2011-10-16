4b7b3d82c1cce53a9568d607f9d30f1961ca60de
Sreeprasad <sreeprasad.sp@gmail.com>
2011-10-16 19:36:44 -0300

    added nth node from last method

--- LeadSinglyLinkedList.java
+++ LeadSinglyLinkedList.java
@@ -2,20 +2,52 @@ public class LeadSinglyLinkedList {
 
 public static void main(String abc[]){
 		SList s = new SList();
-		//s.first(new Integer(11));
 		s.insertInFront(new Integer(11));
-		//System.out.println("first element is "+s.getFirstElement().toString());
 		s.insertInFront(new Integer(14));
-		//System.out.println("first element is "+s.getFirstElement().toString());
 		s.insertInFront(new Integer(14));
 		s.insertInFront(new Integer(11));
 		s.insertInFront(new Integer(12));
 		s.insertInFront(new Integer(19));
-		s.insertInFront(new Integer(14));		
-		System.out.println(s.toString());
-		s.removeDuplicates();
+		s.insertInFront(new Integer(14));
+		
+		/* all values */		
 		System.out.println(s.toString());
 		
-}
+		System.out.println("after removing duplicates ");
+		
+		
+		/* using hashtables */
+		s.removeDuplicatesUsingHashTables();
+		
+		/* all values */
+		System.out.println(s.toString());
+		
+	
+			s.insertInFront(new Integer(11));
+			s.insertInFront(new Integer(14));
+			s.insertInFront(new Integer(14));
+			s.insertInFront(new Integer(11));
+			s.insertInFront(new Integer(12));
+			s.insertInFront(new Integer(19));
+			s.insertInFront(new Integer(14));
+			
+			System.out.println("after adding new items ");
+		
+			/* all values */
+			System.out.println(s.toString());
+			
+			System.out.println("after removing duplicates ");
+			
+		/* removing duplicates without hashtable using runners */
+		s.removeDuplicatesWithoutBufferUsingRunners();
+		
+		/* all values */
+		System.out.println(s.toString());
+
+		/* showing nth node from last. here n=3 */
+		System.out.println("3 node from last is ");
+		System.out.println((s.nthNodeFromLast(3)).toString());
+		
+	}
 
 }
